1227b4c2b23b6f17360d7586c27f7e586b01c8c0
Akbarali <aabduqayumov177@gmail.com>
2025-04-25 09:53:45 +0500

    blog app yaratildi

---
+++ blog/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from  django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta():
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+    
+    objects = models.Manager()
+    published = PublishedManager()
+
+
+posts = Post.objects.filter(status='published')
+p_post = Post.objects.all()
