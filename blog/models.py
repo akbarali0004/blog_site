e1197cc7117203c06e6b7b4d1f89aaa6c2926246
Akbarali <aabduqayumov177@gmail.com>
2025-05-05 10:15:48 +0500

    post-detail ishladi

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from  django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
-
 
 
 class Post(models.Model):
@@ -32,6 +32,12 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
 
-posts = Post.objects.filter(status='published')
-p_post = Post.objects.all()
+
+# posts = Post.objects.filter(status='published')
+# p_post = Post.objects.all()
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import PostListView, post_details
+
+app_name = 'blog'
 
 urlpatterns = [
-    # path('', )
+    path('', PostListView.as_view(), name='list_view'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>', post_details, name='post_detail')
 ]
